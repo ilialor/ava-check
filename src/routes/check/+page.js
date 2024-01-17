0dfc8eaf86dad350ab96c5243af462355e6527dc
Ilia Agafonov <ilia@homeforavatars.com>
2024-01-17 16:42:52 +0300

    remove comments

--- src/routes/check/+page.js
+++ src/routes/check/+page.js
@@ -2,20 +2,15 @@ import { dev } from '$app/environment';
 import { Actor, HttpAgent } from '@dfinity/agent';
 import { idlFactory as doctoken } from './service.did.js';
 
-// ID вашего канистера
 const doctoken_id = 'h5x3q-hyaaa-aaaal-adg6q-cai';
 
-// Создайте HttpAgent для главной сети
 const agent = new HttpAgent({ host: 'https://ic0.app' });
 
-// Создайте актора для вашего канистера
 const doctoken_canister = Actor.createActor(doctoken, { agent, canisterId: doctoken_id });
 
-// Вызовите метод канистера
 doctoken_canister.getDocumentById(38).then(response => {
   console.log(response);
 });
-
 
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
