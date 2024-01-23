73635bb1a051c88d97026e1dd4320ad88c853e53
Ilia Agafonov <ilia@homeforavatars.com>
2024-01-23 08:35:24 +0300

    fix pnpm-lock

--- src/routes/check/+page.js
+++ src/routes/check/+page.js
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
 import { Actor, HttpAgent } from '@dfinity/agent';
-// import { Principal } from '@dfinity/principal';
+import { Principal } from '@dfinity/principal';
 import { idlFactory as rep } from './service_rep.did.js';
 
 const rep_canister = "aoxye-tiaaa-aaaal-adgnq-cai";
@@ -8,7 +8,7 @@ const rep_canister = "aoxye-tiaaa-aaaal-adgnq-cai";
 const agent = new HttpAgent({ host: 'https://ic0.app' });
 
 const rep_canister_actor = Actor.createActor(rep, { agent, canisterId: rep_canister });
-rep_canister_actor.getUserBalance("oa7ab-4elxo-r5ooc-a23ga-lheml-we4wg-z5iuo-ery2n-57uyv-u234p-pae").then(response => {
+rep_canister_actor.getUserBalance(Principal.fromText("oa7ab-4elxo-r5ooc-a23ga-lheml-we4wg-z5iuo-ery2n-57uyv-u234p-pae")).then(response => {
   console.log("Reputation balance: ", response);
 });
 
