cda5f0894264c5a4d04555df002cfb875212454d
Ilia Agafonov <ilia@homeforavatars.com>
2024-01-23 13:53:43 +0300

    replace indexDB to localStorage

--- src/routes/cert/+page.js
+++ src/routes/cert/+page.js
@@ -1,13 +1,62 @@
 import { dev } from '$app/environment';
 import { Actor, HttpAgent } from '@dfinity/agent';
+import { AuthClient } from '@dfinity/auth-client';
 
 import { idlFactory as doctoken } from './service.did.js';
 
+/**
+ * @typedef {'string' | CryptoKeyPair} StoredKey
+ */
+
+const noStorageImpl = {
+  /**
+   * @param {string} key
+   * @returns {Promise<StoredKey|null>}
+   */
+  get(key) {
+    return Promise.resolve(null);
+  },
+  /**
+   * @param {string} key
+   * @param {StoredKey} value
+   * @returns {Promise<void>}
+   */
+  set(key, value) {
+    return Promise.resolve();
+  },
+  /**
+   * @param {string} key
+   * @returns {Promise<void>}
+   */
+  remove(key) {
+    return Promise.resolve();
+  },
+};
 
 const doctoken_id = 'h5x3q-hyaaa-aaaal-adg6q-cai';
 const tokenId = 38;
 
 const agent = new HttpAgent({ host: 'https://ic0.app' });
+
+// Используйте кастомное хранилище, чтобы избежать использования indexedDB
+// const noStorageImpl = {
+//     get(key) {
+//         return Promise.resolve(null);
+//     },
+//     set(key, value) {
+//         return Promise.resolve();
+//     },
+//     remove(key) {
+//         return Promise.resolve();
+//     },
+// };
+
+// Создайте экземпляр AuthClient с кастомным хранилищем
+async function initializeAuthClient() {
+  return await AuthClient.create({
+    storage: noStorageImpl,
+  });
+}
 
 const doctoken_canister = Actor.createActor(doctoken, { agent, canisterId: doctoken_id });
 
@@ -15,11 +64,11 @@ doctoken_canister.getDocumentById(tokenId).then(response => {
   console.log("Doctoken: ", response);
 });
 
+// Инициализация AuthClient
+initializeAuthClient().then(authClient => {
+  // Здесь можно выполнить действия с authClient, например, проверить аутентификацию
+});
 
-// we don't need any JS on this page, though we'll load
-// it in dev so that we get hot module replacement
+// Поддержка HMR и предварительная отрисовка
 export const csr = dev;
-
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
 export const prerender = true;
--- src/routes/cert/+page.svelte
+++ src/routes/cert/+page.svelte
@@ -1,24 +1,24 @@
 <script>
-	import CertCard from "../Cert_card.svelte";
+	import CertCard from '../Cert_card.svelte';
 	import { loginII, logout, isAuthenticated, principalId } from '../../auth.js';
 
-let loggedIn = false;
-isAuthenticated.subscribe(value => {
-  loggedIn = value;
-});
+	let loggedIn = false;
+	isAuthenticated.subscribe((value) => {
+		loggedIn = value;
+	});
 
-let principal = '';
-principalId.subscribe(value => {
-  principal = value;
-});
+	let principal = '';
+	principalId.subscribe((value) => {
+		principal = value;
+	});
 
-function handleLogin() {
-  loginII();
-}
+	function handleLogin() {
+		loginII();
+	}
 
-function handleLogout() {
-  logout();
-}
+	function handleLogout() {
+		logout();
+	}
 </script>
 
 <svelte:head>
@@ -26,19 +26,17 @@ function handleLogout() {
 	<meta name="description" content="User's certificates" />
 </svelte:head>
 
-{ #if loggedIn }
-<div>
-Привет, { principal }!
-<button on:click = { handleLogout } > Выйти</button >
-  </div >
+{#if loggedIn}
+	<div>
+		Привет, {principal}!
+		<button on:click={handleLogout}> Выйти</button>
+	</div>
 {:else}
-<button on:click = { handleLogin } > Войти через Internet Identity</button >
-  {/if}
+	<button on:click={handleLogin}> Войти через Internet Identity</button>
+{/if}
 
 <div class="text-column">
 	<h1>Your certificate:</h1>
 
-
-<CertCard />	
-
+	<CertCard />
 </div>
