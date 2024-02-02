6d8b8e2827278f0990989e156434afda9a287bd7
Ilia Agafonov <ilia@homeforavatars.com>
2024-02-02 08:22:16 +0300

    handle array

--- src/routes/cert/+page.js
+++ src/routes/cert/+page.js
@@ -21,6 +21,7 @@ async function fetchUserDocuments(principalText) {
 
     try {
         const response = await hubCanister.getUserDocuments(principal);
+        console.log("User documents: ", { response });
         return response;
     } catch (error) {
         console.error('Error fetching user documents:', error);
@@ -32,21 +33,28 @@ async function fetchUserDocuments(principalText) {
  * Fetch documents from doctoken canisters for each pair [(CanisterId, DocId)] using getCert with principalText
  * @param {Array<[string, string]>} documentPairs - An array of tuples containing CanisterId and DocId
  * @param {string} principalText - The principal text to pass to getCert
- * @returns {Promise<Array<any>>} A promise that resolves to an array of documents
+ * @returns {Promise<any>} A promise that resolves to an array of documents
  */
 async function fetchDocumentsFromCanisters(documentPairs, principalText) {
     const documents = [];
     for (const [canisterId, docId] of documentPairs) {
         const doctokenCanister = Actor.createActor(doctokenIdlFactory, { agent, canisterId });
+
         try {
-            const document = await doctokenCanister.userCertificates(principalText);
-            documents.push(document);
+            const certificates = await doctokenCanister.userCertificates(principalText);
+            console.log(`Certificates for principal ${principalText} from canister ${canisterId}:`, certificates);
+            if (Array.isArray(certificates)) {
+                documents.push(...certificates); // Spread the array of certificates into the documents array
+            } else {
+                console.error(`Certificates received from canister ${canisterId} are not an array:`, certificates);
+                documents.push(null);
+            }
         } catch (error) {
             console.error(`Error fetching document for principal ${principalText} from canister ${canisterId}:`, error);
             documents.push(null);
         }
-    }
-    return documents;
+        return documents;
+    };
 }
 
 /**
